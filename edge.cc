aae457e993ff7d351a9d87c410aef1d493dd2f04
jiangzhihui <coboy123456@gmail.com>
2013-11-24 21:58:08 +0800

    Init the overall structure for the simulation project

---
+++ edge.cc
@@ -0,0 +1,28 @@
+/*                                                                              
+ ============================================================================
+ Name        : 
+ Author      : Zhihui,Jiang             
+ Email       : coboy123456@gmail.com    
+ Version     : 0.0 
+ Copyright   : Your copyright notice    
+ Description :  
+ ============================================================================
+ */
+
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+#include<map> 
+#include<set>
+#include<deque>
+#include<list>
+#include<algorithm>
+#include<cmath>
+using namespace std; 
+
+int main()
+{
+
+}
+
